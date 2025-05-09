9d33ea02af2fcf4d1e47220026bbd6a2d8784c1d
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-05-09 21:19:22 +0100

    Add Quad utility class: Implement a data class to represent a tuple of four values, enhancing data handling capabilities in the app.

---
+++ app/src/main/kotlin/com/zenithtasks/ui/util/Quad.kt
@@ -0,0 +1,8 @@
+package com.zenithtasks.ui.util
+
+/**
+ * Utility class to represent a tuple of four values.
+ * 
+ * Similar to Kotlin's Pair and Triple, but with four elements.
+ */
+data class Quad<A, B, C, D>(val first: A, val second: B, val third: C, val fourth: D)
